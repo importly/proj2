6809ff672bb6952dcd3144edd5edf2ad886fc397
Aryan Thakur <52215513+importly@users.noreply.github.com>
2026-03-21 13:17:53 -0400

    update lab

--- src/Lab.cpp
+++ src/Lab.cpp
@@ -3,3 +3,41 @@
 //
 
 #include "Lab.h"
+
+
+Lab::Lab(const std::string &college, const std::string &department, const std::string &topic, const std::string &level,
+    const std::string &contact_info, const std::string &synopsis): college(college),
+                                                                   department(department),
+                                                                   topic(topic),
+                                                                   level(level),
+                                                                   contact_info(contact_info),
+                                                                   synopsis(synopsis) {
+}
+
+std::string Lab::get_search_string() const {
+    return college + "_" + department + "_" + topic + "_" + level;
+}
+
+std::string Lab::get_college() {
+    return college;
+}
+
+std::string Lab::get_department() {
+    return department;
+}
+
+std::string Lab::get_topic() {
+    return topic;
+}
+
+std::string Lab::get_level() {
+    return level;
+}
+
+std::string Lab::get_contact_info() {
+    return contact_info;
+}
+
+std::string Lab::get_synopsis() {
+    return synopsis;
+}
--- src/Lab.h
+++ src/Lab.h
@@ -12,6 +12,28 @@ class Lab {
     std::string department;
     std::string topic;
     std::string level;
+
+    std::string contact_info;
+    std::string synopsis;
+
+public:
+
+    Lab(const std::string &college, const std::string &department, const std::string &topic, const std::string &level,
+    const std::string &contact_info, const std::string &synopsis);
+
+    std::string get_search_string() const;
+
+    std::string get_college();
+
+    std::string get_department();
+
+    std::string get_topic();
+
+    std::string get_level();
+
+    std::string get_contact_info();
+
+    std::string get_synopsis();
 };
 
 
