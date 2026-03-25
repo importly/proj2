086298db84250ece1d22cb87d969779008686f2e
Aryan Thakur <52215513+importly@users.noreply.github.com>
2026-03-25 19:06:23 -0400

    fix hashtable issue

--- src/hashtable.h
+++ src/hashtable.h
@@ -15,37 +15,9 @@ class hashtable {
     std::vector<std::vector<std::vector<Lab>>> table;
     //  ^ hashed ^ handle collisions ^ vec of labs
 
-    bool push_back(const std::string& key, const Lab& lab) {
-        if (table.size()/table.capacity() > 0.8) table.resize(table.capacity()*2);
-        auto possible_labs_vec = table[lab.hash()%table.capacity()];
-        bool found = false;
-        for (auto i : possible_labs_vec) {
-            if (key==i[0].get_search_string()) {
-                for (auto j : i) {
-                    if (lab.get_name() == j.get_name()) {
-                        return false;
-                    }
-                }
-                i.push_back(lab);
-                found = true;
-                break;
-            }
-        }
-        if (!found) {
-            possible_labs_vec.push_back({lab});
-        }
-        return true;
-    }
+    bool push_back(const std::string& key, const Lab& lab);
 
-    std::vector<Lab> search(std::string key) {
-        auto possible_labs_vec = table[Lab::string_hash(key)%table.capacity()];
-        for (auto i:possible_labs_vec) {
-            if (i[0].get_search_string() == key) {
-                return i;
-            }
-        }
-        return {};
-    }
+    std::vector<Lab> search(std::string key);
 };
 
 
