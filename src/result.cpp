708fb026e6604acdadad347d1941902d58e1b23e
Bhavika Agarwal <bagarwal1@ufl.edu>
2026-03-27 17:34:26 -0400

    fixed n-ary to take csvb input

--- src/result.cpp
+++ src/result.cpp
@@ -3,14 +3,18 @@
 
 void result::resultScreen() {
     sf::RenderWindow resultWindow(sf::VideoMode(1600, 1200), "Research Lab Finder");
-    resultWindow.clear(sf::Color(76,124,138));
+
+
     while(resultWindow.isOpen()) {
+
         sf::Event event;
         while (resultWindow.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 resultWindow.close();
             }
         }
+
+        resultWindow.clear(sf::Color(76,124,138));
         resultWindow.display();
     }
 }
